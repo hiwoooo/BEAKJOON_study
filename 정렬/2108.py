655929cb24de2ed9240c66ac5555ca9beda2589b
hyewoo <95407936+hiwoooo@users.noreply.github.com>
2022-05-16 20:38:02 +0900

    커밋

--- 정렬/2108.py
+++ 정렬/2108.py
@@ -5,10 +5,36 @@
 최빈값 : N개의 수들 중 가장 많이 나타나는 값
 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 '''
+from collections import Counter
 
 N=int(input())
 arr=[]
 for _ in range(N):
-    arr.append(int(input))
+    arr.append(int(input()))
 
-print(round(sum(arr)/len(arr)))
+def arithmetic_mean(arr):
+    return round(sum(arr)/len(arr))
+
+def median_value(arr):
+    arr.sort()
+    return arr[len(arr)//2]
+
+def Mode(arr):
+    arr.sort()
+    cnt=Counter(arr).most_common()
+    if len(cnt)==1:
+        return cnt[0][0]
+    elif cnt[0][1]==cnt[1][1]:
+        return cnt[1][1]
+    else :
+        return cnt[0][0]
+
+def Range(arr):
+    return max(arr)-min(arr)
+
+print(arithmetic_mean(arr))
+print(median_value(arr))
+print(Mode(arr))
+print(Range(arr))
+
+#python3 실행 시 시간초과, PyPy가 속도가 빠르다고 하여 pypy3로 돌리니 통과.
